88891d8922da1a63b79a8b8294c6541aabb4e42f
koro <koro@localhost>
2013-05-18 17:46:53 +1000

    Added basic controls and test data to GUI

--- ZYXBA_GUI/src/com/carr/mitchell/zyxba_gui/GUI.java
+++ ZYXBA_GUI/src/com/carr/mitchell/zyxba_gui/GUI.java
@@ -1,9 +1,15 @@
 package com.carr.mitchell.zyxba_gui;
 
+import java.awt.Dimension;
 import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class GUI {
 	
@@ -12,11 +18,21 @@ public class GUI {
 	private JButton btnExportToXML;
 	private JButton btnExportToJSON;
 	private JButton btnExportToMSQL;
+        
+        private JMenuBar menuBar;
+        private JMenu menu;
+        private JMenuItem menuItemOpen;
+        private JMenuItem menuItemExportXML;
+        private JMenuItem menuItemExportJSON;
+        private JMenuItem menuItemExportMSQL;
+        
+        private JTable table;
 
-	public static String openCsv = "Open CSV File";
-	public static String exportXML = "Export to XML File";
-	public static String exportJSON = "Export to JSon File";
-	public static String exportMSQL = "Export to MySQL Database";
+	public static final String strMenuFile = "File";
+	public static final String openCsv = "Open CSV File";
+	public static final String exportXML = "Export to XML File";
+	public static final String exportJSON = "Export to JSon File";
+	public static final String exportMSQL = "Export to MySQL Database";
 	
 	private File csv;
 	
@@ -35,6 +51,7 @@ public class GUI {
 	private void instantiateGUI(){
 		
 		frame = new JFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		btnOpen = new JButton(openCsv);
 		btnExportToXML = new JButton(exportXML);
@@ -48,8 +65,63 @@ public class GUI {
 		btnExportToXML.addActionListener(el);
 		btnExportToJSON.addActionListener(el);
 		btnExportToMSQL.addActionListener(el);
+                
+                frame.setMinimumSize(new Dimension(400, 300));
+                
+                menuBar = new JMenuBar();
+                menu = new JMenu(strMenuFile);
+                menu.add(createMenuItem(menuItemOpen, openCsv));
+                menu.add(createMenuItem(menuItemExportXML, exportXML));
+                menu.add(createMenuItem(menuItemExportJSON, exportJSON));
+                menu.add(createMenuItem(menuItemExportMSQL, exportMSQL));
+                menuBar.add(menu);
+                
+                frame.setJMenuBar(menuBar);
+                
+                table = new JTable(testData(), testHeaders());
+                frame.add(new JScrollPane(table));
 		
 	}
+        
+        private Object[][] testData(){
+
+            Object[][] data = {
+                {"Kathy", "Smith",
+                 "Snowboarding", 5, false},
+                {"John", "Doe",
+                 "Rowing", 3, true},
+                {"Sue", "Black",
+                 "Knitting", 2, false},
+                {"Jane", "White",
+                 "Speed reading", 20, true},
+                {"Joe", "Brown",
+                 "Pool", 10, false}
+            };
+            
+            return data;
+            
+        }
+        
+        private String[] testHeaders(){
+
+            String[] columnNames = {"First Name",
+                        "Last Name",
+                        "Sport",
+                        "# of Years",
+                        "Vegetarian"};
+            
+            return columnNames;
+            
+        }
+        
+        private JMenuItem createMenuItem(JMenuItem item, String command){
+            
+            item = new JMenuItem();
+            item.setText(command);
+            
+            return item;
+            
+        }
 	
 	public void display(){
 		frame.setVisible(true);
